59b743ce7ead86345fecbdf447f07f48e164c3b7
GrimProg <vavilovila01@gmail.com>
2025-11-08 17:23:53 +0600

    Изучил @Inject(REQUEST)

--- src/scope/scope.controller.ts
+++ src/scope/scope.controller.ts
@@ -3,10 +3,18 @@ import { ScopeService } from './scope.service';
 
 @Controller('scope')
 export class ScopeController {
-  constructor(private readonly userId: ScopeService) {}
+  constructor(private readonly scopeService: ScopeService) {}
 
   @Get()
   getId() {
-    return this.userId.getId();
+    return this.scopeService.getId();
+  }
+
+  @Get('request')
+  showRequestId() {
+    return {
+      id: this.scopeService.getRequestId(),
+      url: this.scopeService.getRequestURL(),
+    };
   }
 }
--- src/scope/scope.service.ts
+++ src/scope/scope.service.ts
@@ -1,10 +1,25 @@
-import { Injectable, Scope } from '@nestjs/common';
+import { Inject, Injectable, Scope } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
 
-@Injectable({ scope: Scope.TRANSIENT })
+@Injectable({ scope: Scope.REQUEST })
 export class ScopeService {
   private readonly id = Math.random();
+  private readonly requesId: number;
+
+  constructor(@Inject(REQUEST) private request: Request) {
+    this.id = Math.floor(Math.random() * 10000);
+    console.log(`[ScopeServise] новый сервис создан для запроса: ${this.id}`);
+  }
 
   getId() {
     return this.id;
+  }
+
+  getRequestId() {
+    return this.id;
+  }
+
+  getRequestURL() {
+    return this.request.url;
   }
 }
